f4b6a915a5dfb48b2beb22158f7df73881101021
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2019-02-11 12:56:36 +0100

    typewriter - initial commit

---
+++ typewriter/main.js
@@ -0,0 +1,12 @@
+// Typewriter
+
+// init - store/read tge text from div
+//        remove text from div
+//        reset counter
+//        start loop
+
+// loop - find text from 0 to counter
+//        display textpart in html
+//        counter ++
+//        if text.lenght>counter
+//              delay 400ms
