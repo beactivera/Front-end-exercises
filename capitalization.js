38717b2efc60a8cdedccb5985d9a27c327c0fd98
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2019-02-11 21:41:08 +0100

    capitalization on string

---
+++ capitalization.js
@@ -0,0 +1,8 @@
+const str1 = "peTER";
+let firstL = str1.substring(0, 1);
+let otherL = str1.substring(1, str1.length);
+
+let changedStr = firstL.toUpperCase() + otherL.toLowerCase();
+
+console.log(changedStr);
+// let strFinal = str1.indexOf()
