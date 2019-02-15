9e3482f4735d00a441580d1da0e8b5d9cebbdfd3
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2019-02-15 19:52:37 +0100

    counting inside the array - first solution

--- counting-array/count.js
+++ counting-array/count.js
@@ -1,3 +1,15 @@
+//Plan
+
+// init:
+// - set counter = 0
+// - make an empty array
+// - start the loop
+
+// loop:
+// - increment the counter
+// - display the counter
+// - loop the loop -with a delay
+
 let counter;
 
 counter = 0;
@@ -17,41 +29,4 @@ function counting() {
     array.unshift(counter);
     console.log(array);
   }
-}
-
-//Plan
-
-// init:
-// - set counter = 0
-// - make an empty array
-// - start the loop
-
-// loop:
-// - increment the counter
-// - display the counter
-// - loop the loop -with a delay
-
-let counter = 0;
-const arr = [];
-
-loop();
-
-function loop() {
-  // - increment counter
-  counter++;
-
-  // - insert the counter in th ebeggining of the array
-  arr.unshift(counter);
-
-  const MAX_LENGTH_OF_ARRAY = 9;
-  // - make sure that the array doesn't get longer than 9
-  if (arr.lastIndexOf > MAX_LENGTH_OF_ARRAY) {
-    arr.pop();
-  }
-
-  // - display the array
-  console.log(arr);
-
-  // - loop the loop - with a delay
-  setTimeout(loop, 1000);
 }
