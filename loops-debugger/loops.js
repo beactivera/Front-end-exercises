21e05d0221d76e5c933513bc84781b47bfe6b0bf
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2019-02-15 19:35:43 +0100

    created loops

---
+++ loops-debugger/loops.js
@@ -0,0 +1,33 @@
+"use strict";
+
+for (let counter = 0; counter < 10; counter++) {
+  console.log(counter);
+}
+
+// from 0 to 9
+for (let counter = 0; counter < 10; counter++) {
+  console.log(counter);
+}
+
+// // from 1 to 10
+// let counter = 0;
+// while (counter < 11) {
+//   console.log(counter);
+//   counter++;
+// }
+
+// from 10 to 0, followed by the text: “liftoff”  console.log(“liftoff”)
+
+// From 1 to 19, but only odd numbers
+
+//From 1 to 16777216, but doubling each time (2, 4, 8, 16 …)
+// counter^2;
+// for (let i = 1; i < 16777216; i++) {
+//   console.log(i);
+// }
+
+// From 111 to 138, in steps of three
+// counter+=3;
+
+// From 100 to 0 in steps of 10
+// counter-=10
